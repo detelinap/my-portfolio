91f85505dfda96e0dd8046975b5175c4a56cde65
Detelina Palova <palova.detelina@gmail.com>
2023-07-13 21:24:22 +0200

    initial commint

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import NavBar from "./components/NavBar/NavBar";
+import About from "./components/About/About";
+import Contacts from "./components/Contacts/Contacts";
+import Projects from "./components/Projects/Projects";
+import Footer from "./components/Footer/Footer";
+import { Element } from "react-scroll";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <main>
+        <Element name="about" className="section">
+          <About />
+        </Element>
+        <Element name="projects" className="section">
+          <Projects />
+        </Element>
+        <Element name="contact" className="section">
+          <Contacts />
+        </Element>
+      </main>
+      <Footer />
     </div>
   );
 }
